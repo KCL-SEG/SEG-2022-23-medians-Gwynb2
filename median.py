6640b767393a073178356a896c11739d6b55994c
Gwyn <gwyn.barrientos@kcl.ac.uk>
2022-10-21 10:06:14 +0100

    initial median method

--- median.py
+++ median.py
@@ -5,6 +5,7 @@ import statistics
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
+        numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
