532fec443becf3bf3d410c2c5a1c963ddefa47a2
Gwyn <gwyn.barrientos@kcl.ac.uk>
2022-10-21 10:01:59 +0100

    intitial medain method

--- median.py
+++ median.py
@@ -2,14 +2,14 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 import statistics
-
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
-          med = statistics(numbers)
-    break
+        numbers.sort()
+        med = statistics.median(numbers)
+
+        break
 print(med)
